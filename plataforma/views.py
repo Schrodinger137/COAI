a9b53d50600d7bedd184caa312d4abfbdc877234
valicapa <crinaponce02@gmail.com>
2025-07-12 13:48:59 -0600

    Carina, menú y cards

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'plataforma/index.html')
+
+def ejemplo(request):
+     return render(request, 'plataforma/ejemplo.html')
